d664edbbe9421d9123ebba228ca66a7167607848
Yosia Adityo Noviantoro <yosia_17@apps.ipb.ac.id>
2023-10-18 21:38:23 +0700

    refactor: use DDT for negative test login feature

--- src/test/java/cucumber/resource/features/login.feature
+++ src/test/java/cucumber/resource/features/login.feature
@@ -1,28 +1,22 @@
 Feature: Login
-  Scenario: Failed Login Invalid Credentials
-    Given User on Login Page
-    When User input wrong username
-    And User input wrong password
-    And User click button Login
-    Then User get error invalid username or password
-
-  Scenario: Failed Login Empty Username
-    Given User on Login Page
-    When User input empty username
-    And User input password
-    And User click button Login
-    Then User get error username is required
-
-  Scenario: Failed Login Empty Password
-    Given User on Login Page
-    When User input username
-    And User input empty password
-    And User click button Login
-    Then User get error password is required
-
   Scenario: Success Login
     Given User on Login Page
     When User input username
     And User input password
     And User click button Login
     Then User get success login
+
+
+  Scenario Outline: Failed Login Invalid Credentials
+    Given User on Login Page
+    When User input wrong username <username>
+    And User input wrong password <password>
+    And User click button Login
+    Then User get error <errorMessage> message
+
+      Examples:
+        | username      | password     | errorMessage                                                |
+        | lalala        | lalala       | Username and password do not match any user in this service |
+        | standard_user |              | Password is required                                        |
+        |               | secret_sauce | Username is required                                        |
+
--- src/test/java/cucumber/stepDef/StepLogin.java
+++ src/test/java/cucumber/stepDef/StepLogin.java
@@ -16,7 +16,6 @@ public class StepLogin {
     WebDriver driver;
     String baseUrl = "https://www.saucedemo.com/";
 
-
     @Given("User on Login Page")
     public void userOnLoginPage() {
         WebDriverManager.chromedriver().setup();
@@ -26,52 +25,8 @@ public class StepLogin {
         driver.manage().timeouts().implicitlyWait(10, java.util.concurrent.TimeUnit.SECONDS);
         driver.get(baseUrl);
 
-
-
         String loginPageAssert = driver.findElement(By.cssSelector(".login_logo")).getText();
         Assert.assertEquals("Swag Labs", loginPageAssert);
-
-    }
-
-    @When("User input wrong username")
-    public void userInputWrongUsername() {
-        driver.findElement(By.cssSelector("input[data-test=username]")).sendKeys("lalala@gmail.com");
-    }
-
-    @And("User input wrong password")
-    public void userInputWrongPassword() {
-        driver.findElement(By.cssSelector("input[data-test=password]")).sendKeys("lalala");
-    }
-
-    @And("User click button Login")
-    public void userClickButtonLogin() {
-        driver.findElement(By.cssSelector("input[data-test=login-button]")).click();
-    }
-
-    @When("User input empty username")
-    public void userInputEmptyUsername() {
-        driver.findElement(By.cssSelector("input[data-test=username]")).sendKeys("");
-    }
-
-    @And("User input password")
-    public void userInputPassword() {
-        driver.findElement(By.cssSelector("input[data-test=password]")).sendKeys("secret_sauce");
-    }
-
-    @Then("User get error invalid username or password")
-    public void userGetErrorMessage() {
-        String errorMessage = driver.findElement(By.cssSelector("h3[data-test=error]")).getText();
-        Assert.assertEquals("Epic sadface: Username and password do not match any user in this service", errorMessage);
-
-        driver.quit();
-    }
-
-    @Then("User get error username is required")
-    public void userGetErrorUsernameIsRequired() {
-        String errorMessage = driver.findElement(By.cssSelector("h3[data-test=error]")).getText();
-        Assert.assertEquals("Epic sadface: Username is required", errorMessage);
-
-        driver.quit();
     }
 
     @When("User input username")
@@ -79,23 +34,41 @@ public class StepLogin {
         driver.findElement(By.cssSelector("input[data-test=username]")).sendKeys("standard_user");
     }
 
-    @And("User input empty password")
-    public void userInputEmptyPassword() {
-        driver.findElement(By.cssSelector("input[data-test=password]")).sendKeys("");
+    @And("User input password")
+    public void userInputPassword() {
+        driver.findElement(By.cssSelector("input[data-test=password]")).sendKeys("secret_sauce");
     }
 
-    @Then("User get error password is required")
-    public void userGetErrorPasswordIsRequired() {
-        String errorMessage = driver.findElement(By.cssSelector("h3[data-test=error]")).getText();
-        Assert.assertEquals("Epic sadface: Password is required", errorMessage);
-
-        driver.quit();
+    @And("User click button Login")
+    public void userClickButtonLogin() {
+        driver.findElement(By.cssSelector("input[data-test=login-button]")).click();
     }
+
 
     @Then("User get success login")
     public void userGetSuccessLogin() {
         String titleHomePage = driver.findElement(By.cssSelector(".title")).getText();
         Assert.assertEquals("Products", titleHomePage);
+
+        driver.quit();
+    }
+
+//    ======= DDT =======
+    @When("User input wrong username {}")
+    public void userInputWrongUsername(String arg0) {
+        driver.findElement(By.cssSelector("input[data-test=username]")).sendKeys(arg0);
+    }
+
+    @And("User input wrong password {}")
+    public void userInputWrongPassword(String arg0) {
+        driver.findElement(By.cssSelector("input[data-test=password]")).sendKeys(arg0);
+    }
+
+    @Then("User get error {} message")
+    public void userGetErrorMessage(String arg0) {
+        String errorMessage = driver.findElement(By.cssSelector("h3[data-test=error]")).getText();
+        String expectedErrorMessage = "Epic sadface: " + arg0;
+        Assert.assertEquals(expectedErrorMessage, errorMessage);
 
         driver.quit();
     }
