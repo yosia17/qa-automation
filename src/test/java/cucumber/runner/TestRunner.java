3df31205c92d8881ca3510e55d7947b07f4d8733
Yosia Adityo Noviantoro <yosia_17@apps.ipb.ac.id>
2023-10-20 11:22:42 +0700

    add report testing result

--- src/test/java/cucumber/runner/TestRunner.java
+++ src/test/java/cucumber/runner/TestRunner.java
@@ -6,10 +6,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "cucumber/resource/features",
-        glue = "cucumber/stepDef",
-        plugin = {"html:target/Html_report.html"}
+        features = "src/test/java/cucumber/resource/features",
+        glue = "cucumber.stepDef",
+        plugin = {"pretty","html:src/test/report.html"}
 )
 
-public class Runner {
+public class TestRunner {
 }
